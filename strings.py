f7fd9b5dcd3e2980a05a8634bdc21a264f3b9e4d
Kim Min Jung <sallymj3857>
2020-10-17 20:42:22 +0900

    [Add]strings.py

---
+++ strings.py
@@ -0,0 +1,43 @@
+#이건 뭐에여
+a = "김민정"
+print(a)
+
+#이건 또 뭐에요
+name = "hi "
+name2 = "hello "
+name3 = "my name is min jung kim"
+print(name + name2 + name3)
+
+#이건 저런 거에요
+d = 2
+f = 3
+print(d * f)
+w = 6
+e = 3
+print(w / e)
+q= 7
+r= 4
+print(q // r)
+b= 3
+c= 4
+print(b ** c)
+z= """Hello world """
+x= 'python is fun '
+v= "Life is too short, you need python "
+print(z + x + v)
+food = "Python's favorite food is peal"
+print(food)
+k = "Python"
+print( k * 2)
+y = "Life is too short, You need Python"
+print( y[3] )
+print( y[-9] )
+print( y[0] + y[1] + y[2] + y[3] )
+print( y[0:4] )
+print( y[19:] )
+
+
+
+
+
+
